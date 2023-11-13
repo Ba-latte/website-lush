3349fbb284bb5d68854db9319d84458e9b2ffecb
Bara <youj_lim@naver.com>
2023-11-13 18:24:49 +0900

    fix : 스토어 버튼 수정, 미니창 온/오프 기능 추가
    1.퀵메뉴의 스토어 버튼 태그 수정
    -어떤 곳으로 이동하는 게 아니라 클릭하면 미니창 온/오프하는 특정 목적을 가지고 유도하는 것이므로 버튼 태그로 수정
    2.QR코드 안내하는 미니창 열려있다가 x버튼 누르면 닫히고, 스토어 아이콘 누르면 열리게 만들기

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ window.addEventListener("DOMContentLoaded", ()=>{
   // init();
   // render();
 
+  // 스와이퍼 배너
   const swiper = new Swiper(".lushInfo__banner", {
     slidesPerView: "auto",
     spaceBetween: 80,
   });
 
+  // QR코드 미니창 열기/닫기
+  const qrBox = document.querySelector(".qrBox");
+  const closeBtn = document.querySelector(".qrBox>button");
+  const storeBtn = document.querySelector(".goStore");
+  closeBtn.addEventListener("click", ()=>{
+    qrBox.style.display="none";
+  });
+  storeBtn.addEventListener("click", ()=>{
+    qrBox.style.display="flex";
+  });
 
+  // 롤링 배너
   const rollingBanner = document.querySelector(".rollingBanner .wrapper");
   rollingBanner.id = "wrapper01";
   const newWrapper02 = rollingBanner.cloneNode(true);
