88258e0016c905399d1ab09a8042461905621b17
Bara <youj_lim@naver.com>
2023-11-13 11:45:24 +0900

    fix : '러쉬 소개' 섹션 스와이퍼 배너로 변경
    1.스와이퍼 배너로 변경
    2.마우스 커서 변경
    3.배너 박스 왼쪽에서 슬라이드 보이지 않게 숨김 처리

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import { init, render } from "./js/canvasContainers/MoveGradient.js";
+// import { init, render } from "./js/canvasContainers/MoveGradient.js";
 
 // 윈도우 리사이즈시 캔버스 초기화 함수 실행
-window.addEventListener("resize", init);
+// window.addEventListener("resize", init);
 
 // 윈도우 로드시 캔버스 초기화, 렌더 함수 실행
-window.addEventListener("load", ()=>{
-  init();
-  render();
-});
+window.addEventListener("DOMContentLoaded", ()=>{
+  // init();
+  // render();
 
+  const swiper = new Swiper(".lushInfo__banner", {
+    slidesPerView: "auto",
+    spaceBetween: 80,
+  });
+
+});
