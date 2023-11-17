88c694eecc756c95600839d3f2478c7656cdf136
Bara <youj_lim@naver.com>
2023-11-17 15:46:15 +0900

    add : 본문으로 바로가기 추가
    1.본문으로 바로가기 박스 생성
    2.바로가기 기능 추가

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-// import { init, render } from "./js/canvasContainers/MoveGradient.js";
-
-// 윈도우 리사이즈시 캔버스 초기화 함수 실행
-// window.addEventListener("resize", init);
-
-// 윈도우 로드시 캔버스 초기화, 렌더 함수 실행
+// 윈도우 로드시 실행
 window.addEventListener("DOMContentLoaded", ()=>{
-  // init();
-  // render();
 
   // 러쉬 소개 스와이퍼
   const swiper = new Swiper(".lushInfo__banner", {
