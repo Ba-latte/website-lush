6b439fe5fbe752c8c97812f0a68b9b0fa6405ff5
Bara <youj_lim@naver.com>
2023-11-29 19:27:46 +0900

    add : 스크롤 방향 감지하여 헤더 보이기/숨기기 기능 추가

--- js/common.js
+++ js/common.js
@@ -1,25 +1,77 @@
 // 공통 부분
+console.log("공통 부분 js 로딩 완료");
 
-$(()=>{
-  console.log("공통 부분 js 로딩 완료");
-  
-  const window_height = $(window).height();
-  const contentBox_height = $("main>section:first-of-type>.contentBox").height();
-  const check_height = contentBox_height + (window_height * 0.2);
-  console.log(check_height);
-  
-  // 스크롤시 헤더에 active 클래스 부여해서 활성화
-  window.addEventListener("scroll", ()=>{
-    // console.log(window.scrollY);
-    if(window.scrollY >= check_height){
-      console.log("넘었어!");
-      // 헤더에 클래스 active를 부여해서 색상 반전
-      $("header").addClass("active");
-    }
-    else{
-      console.log("안 넘었어!");
-      // 헤더에서 클래스 active 제거
-      $("header").removeClass("active");
-    }
-  });
+const window_height = $(window).height();
+// 최상단 배너의 콘텐츠 박스 높이
+const contentBox_height = $("main>section:first-of-type>.contentBox").height();
+// 스크롤 넘어간 것 감지할 높이값
+const check_height = contentBox_height + (window_height * 0.2);
+
+function handleHeaderActive(){
+  // 최상단 배너의 콘텐츠 박스의 위치 감지하여 넘어가는 경우
+  if(window.scrollY >= check_height){
+    // console.log("넘었어!");
+    // 헤더에 클래스 active를 부여해서 색상 반전
+    $("header").addClass("active");
+  }
+  else{
+    // console.log("안 넘었어!");
+    // 헤더에서 클래스 active 제거
+    $("header").removeClass("active");
+  }
+}
+
+
+// 스크롤 방향 감지 위한, 스크롤 위치값 담기
+let prevScroll = window.scrollY || 0;
+// 스크롤 방향값 담기
+let direction;
+function handleHeaderHide(){
+  // 현재 스크롤 위치 감지
+  let currentScroll = window.scrollY;
+  // 이전 스크롤 위치값에서 현재 스크롤 위치값 빼서, 스크롤 이동 방향 결정하기 (1 : 내려가는 방향 / -1 : 올라가는 방향)
+  direction = currentScroll - prevScroll > 0 ? 1 : -1;
+  // 이전 스크롤 위치값 업데이트
+  prevScroll = currentScroll;
+  // console.log("방향: ", direction);
+
+  // 올라가는 경우
+  if(direction == -1){
+    // 헤더 보이기
+    $("header").css({
+      transform: "translateY(0px)",
+    });
+    $(".lnb").addClass("extension");
+  }
+  // 내려가는 경우
+  else{
+    $(".lnb").removeClass("extension");
+    // 헤더 숨기기
+    $("header").css({
+      transform: "translateY(-71px)",
+    })
+  }
+}
+
+
+function handleLnbActive(){
+  const lnb_height = $(".lnb").outerHeight() / 2;
+  // 윈도우 높이(최상단 배너의 높이)를 넘어가는 경우
+  if(window.scrollY >= window_height - $("header").outerHeight() + lnb_height){
+    console.log("최상단 배너 넘어갔음!");
+    // lnb 박스에 클래스 active 부여해서 화면에 고정
+    $(".lnb").addClass("active");
+    handleHeaderHide();
+  }
+  else{
+    // lnb 박스에서 클래스 active 제거
+    $(".lnb").removeClass("active");
+  }
+}
+
+// 스크롤시 헤더에 active 클래스 부여해서 활성화
+window.addEventListener("scroll", ()=>{
+  // console.log(window.scrollY);
+  handleHeaderActive();
+  handleLnbActive();
 });
